ea9128076da5375cc068c7c0427872c9ee7c5085
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-05 22:08:29 +0100

    Update, deur trigger binnen en buiten

---
+++ Assets/Scripts/DoorInsideOutsideTrigger/Trigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trigger : MonoBehaviour
+{
+
+  public int sceneBuildIndex; 
+   
+  private void OnTriggerEnter2D(Collider2D other) {
+
+    if(other.tag == "Character") {
+    SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+    } 
+  }
+}
