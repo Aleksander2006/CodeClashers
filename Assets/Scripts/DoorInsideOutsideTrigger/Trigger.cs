2ba233a1f13f3050007c95b426f1ef155dfde141
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-06 16:39:26 +0100

    Update Fade-Out

--- Assets/Scripts/DoorInsideOutsideTrigger/Trigger.cs
+++ Assets/Scripts/DoorInsideOutsideTrigger/Trigger.cs
@@ -6,14 +6,12 @@ using UnityEngine.SceneManagement;
 public class Trigger : MonoBehaviour
 {
 
-  public Animator Crossfade;
   public int sceneBuildIndex; 
    
   private void OnTriggerEnter2D(Collider2D other) {
 
     if(other.tag == "Character") {
     SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-    Crossfade.SetTrigger("Start");
     } 
   }
 }
