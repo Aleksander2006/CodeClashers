b5c7263cb9bd5f4d2cafd854aea55b1ab5f3d6ce
SuperGamer-NL <archeolooglucas@gmail.com>
2024-03-13 14:40:44 +0100

    cacheclear

---
+++ Assets/Scripts/DoorInsideOutsideTrigger/Trigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trigger : MonoBehaviour
+{
+  public Animator Crossfade;
+  public int sceneBuildIndex; 
+
+  private void OnTriggerEnter2D(Collider2D other) {
+      if(other.tag == "Character") {
+          StartCoroutine(FadeDelay());
+          Crossfade.SetTrigger("Go");
+      } 
+  }
+
+  private IEnumerator FadeDelay() {
+      yield return new WaitForSeconds(1);
+      SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+  }
+}
