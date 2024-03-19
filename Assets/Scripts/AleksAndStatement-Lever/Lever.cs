69513e38a86ca2956b59cf687ad4860451a5cad1
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-19 22:36:20 +0100

    Lampje-aan als de 2 levers aanstaan

--- Assets/Scripts/AleksAndStatement-Lever/Lever.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever.cs
@@ -12,48 +12,75 @@ public class Lever : MonoBehaviour
 //private GameObject Lever2;
 //private GameObject Lever4;
 
-public SpriteRenderer Lever1;
-public SpriteRenderer Lever2;
-public SpriteRenderer Lever3;
-public SpriteRenderer Lever4;
-public SpriteRenderer Lampje1;
-public SpriteRenderer Lampje2;
+//public SpriteRenderer Lever1;
+//public SpriteRenderer Lever2;
+//public SpriteRenderer Lever3;
+//public SpriteRenderer Lever4;
+//public SpriteRenderer Lampje1;
+//public SpriteRenderer Lampje2;
 
+    public GameObject Object;
+    public Transform Lever1;
+    public Transform Lever2;
 
-    // Start is called before the first frame update
+//Hendel weg- UIT
+    public float zValue = 1f;
+//Hendel komt-AAN
+    public float zValue2 = -1f;
+
+    private bool isLeverOn = false;
+
     void Start()
     {
-       
+      Lever1.GetComponent<Transform>();
+      Lever2.GetComponent<Transform>();
+      //Lever3.GetComponent<Transform>();
+      //Lever4.GetComponent<Transform>();
+      
+      //Lampje1.GetComponent<Transform>();
+      //Lampje2.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    if (Input.GetKey("e")){
-        Lampje1.enabled = false;
-        Lampje2.enabled = true;
-        }  
+
+// private void OnTriggerEnter2D(Collider2D other) {
+     // if(other.tag == "Character") {
+     //     
+     // } 
+
+ //
+
+//if (Gameobject = true && Gameobject = true){
+ 
+//}
+
+ if (Input.GetKeyDown(KeyCode.E)){
+
+    if(isLeverOn)
+    {
+    //Wanneer die uitstaat en aanmoet
+            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue);
+            Lever1.position = newPosition1;
+
+            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue2);
+            Lever2.position = newPosition2; 
+    }
+    //Wanneer die uit aan staat en uit moet
+    else {
+            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue2);
+            Lever1.position = newPosition1;
+
+            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue);
+            Lever2.position = newPosition2; 
+    }
+
+    }
+
+    isLeverOn = !isLeverOn;
+               
     }
 
 
-
-
 }
-
-//if (Lever2.GetComponent<Transform>()){
-        //    
-        //}
-//
-        //if (Lever4.GetComponent<Transform>()){
-        //    
-        //}
-//
-//
-        //if (Lever2.GetComponent<Transform>() && Lever4.GetComponent<Transform>()) {
-        //    
-        //Lampje1.GetComponent<Transform>();
-        //}
-        //else
-        //{
-    //
-        //}
