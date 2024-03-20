89c69f34f4d086c1c96f7d3bc825315229aa8b0f
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-20 18:20:48 +0100

    De lampjes, kunnen nu individueel aan

--- Assets/Scripts/AleksAndStatement-Lever/Lever.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever.cs
@@ -8,79 +8,91 @@ using UnityEngine.UIElements;
 
 public class Lever : MonoBehaviour
 {
-//private GameObject Lampje1;
-//private GameObject Lever2;
-//private GameObject Lever4;
+        //private GameObject Lampje1;
+        //private GameObject Lever2;
+        //private GameObject Lever4;
+            public Transform Lever1;
+            
+            public Transform Lever2;
 
-//public SpriteRenderer Lever1;
-//public SpriteRenderer Lever2;
-//public SpriteRenderer Lever3;
-//public SpriteRenderer Lever4;
-//public SpriteRenderer Lampje1;
-//public SpriteRenderer Lampje2;
+            public Transform Lampje1;
+            public Transform Lampje2;
 
-    public GameObject Object;
-    public Transform Lever1;
-    public Transform Lever2;
+        //Hendel weg- UIT
+            public float zValue = 1f;
+        //Hendel komt-AAN
+            public float zValue2 = -1f;
 
-//Hendel weg- UIT
-    public float zValue = 1f;
-//Hendel komt-AAN
-    public float zValue2 = -1f;
+            private bool isLeverOn = false;
+            private bool IsCharacterInside = false;
 
-    private bool isLeverOn = false;
+            //Lampje uit-weg
+            private GameObject lever;
+            
 
-    void Start()
-    {
-      Lever1.GetComponent<Transform>();
-      Lever2.GetComponent<Transform>();
-      //Lever3.GetComponent<Transform>();
-      //Lever4.GetComponent<Transform>();
-      
-      //Lampje1.GetComponent<Transform>();
-      //Lampje2.GetComponent<Transform>();
+            void Start(){
+            Lever1.GetComponent<Transform>();
+            Lever2.GetComponent<Transform>();
+            }
+
+            void OnTriggerEnter2D(Collider2D other) {
+                if(other.tag == "Character") {
+                Debug.Log("Inside");
+                IsCharacterInside = true;
+                }
+            }
+
+            void OnTriggerExit2D(Collider2D other) {
+                if(other.tag == "Character") {
+                Debug.Log("Outside");
+                IsCharacterInside = false;
+                }
+            }
+            
+ void Update() {
+
+        if (IsCharacterInside){
+            if(Input.GetKeyDown(KeyCode.E)) { 
+                if(isLeverOn)
+                        {
+                        //Wanneer die uitstaat en aanmoet
+                            Debug.Log("Hij staat Aan");
+                            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue);
+                            Lever1.position = newPosition1;
+
+                            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue2);
+                            Lever2.position = newPosition2;
+                            lever.SetActive(true);
+                            
+                                
+                        }
+                        //Wanneer die uit aan staat en uit moet
+                        else {
+                            Debug.Log("Hij staat Uit");
+                            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue2);
+                            Lever1.position = newPosition1;
+
+                            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue);
+                            Lever2.position = newPosition2;
+                            lever.SetActive(false);             
+                        }
+                
+                }
+        }
+
+            isLeverOn = !isLeverOn;
+
+                    
+
+            if(!isLeverOn){
+
+                    Vector3 newPosition1 = new Vector3(Lampje1.position.x, Lampje1.position.y, zValue);
+                    Lampje1.position = newPosition1;
+
+                    Vector3 newPosition2 = new Vector3(Lampje2.position.x, Lampje2.position.y, zValue2);
+                    Lampje2.position = newPosition2;
+
+                }    
+            
+        }   
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-// private void OnTriggerEnter2D(Collider2D other) {
-     // if(other.tag == "Character") {
-     //     
-     // } 
-
- //
-
-//if (Gameobject = true && Gameobject = true){
- 
-//}
-
- if (Input.GetKeyDown(KeyCode.E)){
-
-    if(isLeverOn)
-    {
-    //Wanneer die uitstaat en aanmoet
-            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue);
-            Lever1.position = newPosition1;
-
-            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue2);
-            Lever2.position = newPosition2; 
-    }
-    //Wanneer die uit aan staat en uit moet
-    else {
-            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue2);
-            Lever1.position = newPosition1;
-
-            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue);
-            Lever2.position = newPosition2; 
-    }
-
-    }
-
-    isLeverOn = !isLeverOn;
-               
-    }
-
-
-}
