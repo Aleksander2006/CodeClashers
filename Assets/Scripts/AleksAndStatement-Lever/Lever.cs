d1ff0ea0ff68d80a21faf3ab9126912615c330e2
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-21 12:49:54 +0100

    1 lampje gaat aan

--- Assets/Scripts/AleksAndStatement-Lever/Lever.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever.cs
@@ -9,21 +9,15 @@ using UnityEngine.UIElements;
 public class Lever : MonoBehaviour
 {
     public Transform Lever1;
-    
-    public Transform Lever2;
-
     public Transform Lampje1;
     public Transform Lampje2;
-
-    //Hendel weg- UIT
     public float zValue = 1f;
-    //Hendel komt-AAN
-    public float zValue2 = -1f;
-
+    public float zValue2 = -1f; 
     private bool isLeverOn = false;
     private bool IsCharacterInside = false;
-    
 
+    public ScriptReference script;
+    
     void Start(){
         Lever1.GetComponent<Transform>();
     }
@@ -45,21 +39,35 @@ public class Lever : MonoBehaviour
     void Update() {
         if (IsCharacterInside){
             if(Input.GetKeyDown(KeyCode.E)) { 
-                Debug.Log("INgedrukt");
+                Debug.Log("Ingedrukt");
                 if(isLeverOn) {
                     //Wanneer die uitstaat en aanmoet
-                    Debug.Log("Lever 3-4 Hij staat Aan");
+                    Debug.Log("Lever 1 staat AAN");
                     
                     transform.localRotation = Quaternion.Euler(0, 180, 180);
 
                 } else {
-                    Debug.Log("Lever 3-4 Hij staat Uit");
+                    Debug.Log("Lever 1 staat UIT");
                    
                     transform.localRotation = Quaternion.Euler(0, 0, 0);
                                 
                 }
-
                 isLeverOn = !isLeverOn;
+                
+                if(!isLeverOn){
+                    Vector3 newPosition1 = new Vector3(Lampje1.position.x, Lampje1.position.y, zValue);
+                    Lampje1.position = newPosition1;
+                    
+                    Vector3 newPosition2 = new Vector3(Lampje2.position.x, Lampje2.position.y, zValue2);
+                    Lampje2.position = newPosition2;
+
+                } else {
+                    Vector3 newPosition1 = new Vector3(Lampje1.position.x, Lampje1.position.y, zValue2);
+                    Lampje1.position = newPosition1;
+                    
+                    Vector3 newPosition2 = new Vector3(Lampje2.position.x, Lampje2.position.y, zValue);
+                    Lampje2.position = newPosition2;       
+                }
             }
         }
     }   
--- Assets/Scripts/AleksAndStatement-Lever/Lever2.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever2.cs
@@ -7,7 +7,8 @@ public class Lever2 : MonoBehaviour
     public Transform Lever3;
     public Transform Lampje1;
     public Transform Lampje2;
-
+    public float zValue = 1f;
+    public float zValue2 = -1f; 
     private bool isLeverOn = false;
     private bool IsCharacterInside = false;
     
@@ -32,20 +33,34 @@ public class Lever2 : MonoBehaviour
     void Update() {
         if (IsCharacterInside){
             if(Input.GetKeyDown(KeyCode.E)) { 
-                Debug.Log("INgedrukt");
+                Debug.Log("Ingedrukt");
                 if(isLeverOn) {
                     //Wanneer die uitstaat en aanmoet
-                    Debug.Log("Lever 3-4 Hij staat Aan");
+                    Debug.Log("Lever 3 Hij staat AAN");
 
                     transform.localRotation = Quaternion.Euler(0, 180, 180);
                     
                 } else {
-                    Debug.Log("Lever 3-4 Hij staat Uit");
+                    Debug.Log("Lever 3 Hij staat UIT");
 
                     transform.localRotation = Quaternion.Euler(0, 0, 0);             
                 }
-
                 isLeverOn = !isLeverOn;
+                if(!isLeverOn){
+                    Vector3 newPosition1 = new Vector3(Lampje1.position.x, Lampje1.position.y, zValue);
+                    Lampje1.position = newPosition1;
+                    
+                    Vector3 newPosition2 = new Vector3(Lampje2.position.x, Lampje2.position.y, zValue2);
+                    Lampje2.position = newPosition2;
+
+                } else {
+                    Vector3 newPosition1 = new Vector3(Lampje1.position.x, Lampje1.position.y, zValue2);
+                    Lampje1.position = newPosition1;
+                    
+                    Vector3 newPosition2 = new Vector3(Lampje2.position.x, Lampje2.position.y, zValue);
+                    Lampje2.position = newPosition2;
+                        
+                }
             }
         }
     }   
