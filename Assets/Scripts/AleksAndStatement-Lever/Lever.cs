0832ed320c4126fc0e2e6276f70ee91e26e38187
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-21 10:28:14 +0100

    Lever werkt

--- Assets/Scripts/AleksAndStatement-Lever/Lever.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever.cs
@@ -8,77 +8,69 @@ using UnityEngine.UIElements;
 
 public class Lever : MonoBehaviour
 {
-            public Transform Lever1;
+    public Transform Lever1;
+    
+    public Transform Lever2;
+
+    public Transform Lampje1;
+    public Transform Lampje2;
+
+    //Hendel weg- UIT
+    public float zValue = 1f;
+    //Hendel komt-AAN
+    public float zValue2 = -1f;
+
+    private bool isLeverOn = false;
+    private bool IsCharacterInside = false;
+
+    //Lampje uit-weg
+    //private GameObject lever;
+    
+
+    void Start(){
+        Lever1.GetComponent<Transform>();
+        Lever2.GetComponent<Transform>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Character") {
+            Debug.Log("Inside");
+            IsCharacterInside = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "Character") {
+            Debug.Log("Outside");
+            IsCharacterInside = false;
+        }
+    }
             
-            public Transform Lever2;
-
-            public Transform Lampje1;
-            public Transform Lampje2;
-
-        //Hendel weg- UIT
-            public float zValue = 1f;
-        //Hendel komt-AAN
-            public float zValue2 = -1f;
-
-            private bool isLeverOn = false;
-            private bool IsCharacterInside = false;
-
-            //Lampje uit-weg
-            //private GameObject lever;
-            
-
-            void Start(){
-            Lever1.GetComponent<Transform>();
-            Lever2.GetComponent<Transform>();
-            }
-
-            void OnTriggerEnter2D(Collider2D other) {
-                if(other.tag == "Character") {
-                Debug.Log("Inside");
-                IsCharacterInside = true;
-                }
-            }
-
-            void OnTriggerExit2D(Collider2D other) {
-                if(other.tag == "Character") {
-                Debug.Log("Outside");
-                IsCharacterInside = false;
-                }
-            }
-            
- void Update() {
-
+    void Update() {
         if (IsCharacterInside){
             if(Input.GetKeyDown(KeyCode.E)) { 
-                if(isLeverOn)
-                        {
-                        //Wanneer die uitstaat en aanmoet
-                            Debug.Log("Hij staat Aan");
-                            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue);
-                            Lever1.position = newPosition1;
+                Debug.Log("INgedrukt");
+                if(isLeverOn) {
+                    //Wanneer die uitstaat en aanmoet
+                    Debug.Log("Lever 3-4 Hij staat Aan");
+                    
+                    transform.localRotation = Quaternion.Euler(0, 180, 180);
 
-                            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue2);
-                            Lever2.position = newPosition2;
-                            //lever.SetActive(true);
-                            
-                                
-                        }
-                        
-                        //Wanneer die uit aan staat en uit moet
-                        else {
-                            Debug.Log("Hij staat Uit");
-                            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue2);
-                            Lever1.position = newPosition1;
-
-                            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue);
-                            Lever2.position = newPosition2;
-                            //lever.SetActive(false);             
-                        }        
-
+                    //lever.SetActive(true);
+                } else {
+                    Debug.Log("Lever 3-4 Hij staat Uit");
+                    //Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue2);
+                    //Lever1.position = newPosition1;
+//
+                    //Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue);
+                    //Lever2.position = newPosition2;
+//
+                    transform.localRotation = Quaternion.Euler(0, 0, 0);
+                    //lever.SetActive(false);             
                 }
-        }
 
-            isLeverOn = !isLeverOn;    
-                         
-        }   
-    }
+                isLeverOn = !isLeverOn;
+            }
+        }
+    }   
+}
--- Assets/Scripts/AleksAndStatement-Lever/Lever2.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever2.cs
@@ -4,77 +4,58 @@ using UnityEngine;
 
 public class Lever2 : MonoBehaviour
 {
-            public Transform Lever3;
+    public Transform Lever3;
+    
+    //public Transform Lever4;
+
+    public Transform Lampje1;
+    public Transform Lampje2;
+
+    private bool isLeverOn = false;
+    private bool IsCharacterInside = false;
+
+    //Lampje uit-weg
+    //private GameObject lever;
+    
+
+    void Start(){
+        Lever3.GetComponent<Transform>();
+        //Lever4.GetComponent<Transform>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Character") {
+            Debug.Log("Inside");
+            IsCharacterInside = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "Character") {
+            Debug.Log("Outside");
+            IsCharacterInside = false;
+        }
+    }
             
-            public Transform Lever4;
-
-            public Transform Lampje1;
-            public Transform Lampje2;
-
-        //Hendel weg- UIT
-            public float zValue = 1f;
-        //Hendel komt-AAN
-            public float zValue2 = -1f;
-
-            private bool isLeverOn = false;
-            private bool IsCharacterInside = false;
-
-            //Lampje uit-weg
-            //private GameObject lever;
-            
-
-            void Start(){
-            Lever3.GetComponent<Transform>();
-            Lever4.GetComponent<Transform>();
-            }
-
-            void OnTriggerEnter2D(Collider2D other) {
-                if(other.tag == "Character") {
-                Debug.Log("Inside");
-                IsCharacterInside = true;
-                }
-            }
-
-            void OnTriggerExit2D(Collider2D other) {
-                if(other.tag == "Character") {
-                Debug.Log("Outside");
-                IsCharacterInside = false;
-                }
-            }
-            
- void Update() {
-
+    void Update() {
         if (IsCharacterInside){
             if(Input.GetKeyDown(KeyCode.E)) { 
-                if(isLeverOn)
-                        {
-                        //Wanneer die uitstaat en aanmoet
-                            Debug.Log("Lever 3-4 Hij staat Aan");
-                            Vector3 newPosition1 = new Vector3(Lever3.position.x, Lever3.position.y, zValue);
-                            Lever3.position = newPosition1;
+                Debug.Log("INgedrukt");
+                if(isLeverOn) {
+                    //Wanneer die uitstaat en aanmoet
+                    Debug.Log("Lever 3-4 Hij staat Aan");
 
-                            Vector3 newPosition2 = new Vector3(Lever4.position.x, Lever4.position.y, zValue2);
-                            Lever4.position = newPosition2;
-                            //lever.SetActive(true);
-                            
-                                
-                        }
-                        
-                        //Wanneer die uit aan staat en uit moet
-                        else {
-                            Debug.Log("Lever 3-4 Hij staat Uit");
-                            Vector3 newPosition1 = new Vector3(Lever3.position.x, Lever3.position.y, zValue2);
-                            Lever3.position = newPosition1;
+                    transform.localRotation = Quaternion.Euler(0, 180, 180);
+                    
+                } else {
+                    Debug.Log("Lever 3-4 Hij staat Uit");
 
-                            Vector3 newPosition2 = new Vector3(Lever4.position.x, Lever4.position.y, zValue);
-                            Lever4.position = newPosition2;
-                            //lever.SetActive(false);             
-                        }        
-
+                    transform.localRotation = Quaternion.Euler(0, 0, 0);
+                               
                 }
-        }
 
-            isLeverOn = !isLeverOn;
-                         
-        }   
-    }
+                isLeverOn = !isLeverOn;
+            }
+        }
+    }   
+}
