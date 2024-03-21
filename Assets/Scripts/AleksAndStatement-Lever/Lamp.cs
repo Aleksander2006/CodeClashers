3f8f07167ce93df5178ede0d3c42ac6fc669785f
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-21 09:28:16 +0100

    Console errors weg en aparte scripts van levers

--- Assets/Scripts/AleksAndStatement-Lever/Lamp.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lamp.cs
@@ -13,7 +13,17 @@ public class Lamp : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {  
+    { 
+
+        //if(isLeverOn){
+//
+    //            Vector3 newPosition1 = new Vector3(Lampje1.position.x, Lampje1.position.y, zValue);
+    //            Lampje1.position = newPosition1;
+//
+    //            Vector3 newPosition2 = new Vector3(Lampje2.position.x, Lampje2.position.y, zValue2);
+    //            Lampje2.position = newPosition2;
+//
+    //} 
     }
 
 
--- Assets/Scripts/AleksAndStatement-Lever/Lever2.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever2.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO.Compression;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class Lever : MonoBehaviour
+public class Lever2 : MonoBehaviour
 {
-            public Transform Lever1;
+            public Transform Lever3;
             
-            public Transform Lever2;
+            public Transform Lever4;
 
             public Transform Lampje1;
             public Transform Lampje2;
@@ -28,8 +24,8 @@ public class Lever : MonoBehaviour
             
 
             void Start(){
-            Lever1.GetComponent<Transform>();
-            Lever2.GetComponent<Transform>();
+            Lever3.GetComponent<Transform>();
+            Lever4.GetComponent<Transform>();
             }
 
             void OnTriggerEnter2D(Collider2D other) {
@@ -53,12 +49,12 @@ public class Lever : MonoBehaviour
                 if(isLeverOn)
                         {
                         //Wanneer die uitstaat en aanmoet
-                            Debug.Log("Hij staat Aan");
-                            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue);
-                            Lever1.position = newPosition1;
+                            Debug.Log("Lever 3-4 Hij staat Aan");
+                            Vector3 newPosition1 = new Vector3(Lever3.position.x, Lever3.position.y, zValue);
+                            Lever3.position = newPosition1;
 
-                            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue2);
-                            Lever2.position = newPosition2;
+                            Vector3 newPosition2 = new Vector3(Lever4.position.x, Lever4.position.y, zValue2);
+                            Lever4.position = newPosition2;
                             //lever.SetActive(true);
                             
                                 
@@ -66,19 +62,19 @@ public class Lever : MonoBehaviour
                         
                         //Wanneer die uit aan staat en uit moet
                         else {
-                            Debug.Log("Hij staat Uit");
-                            Vector3 newPosition1 = new Vector3(Lever1.position.x, Lever1.position.y, zValue2);
-                            Lever1.position = newPosition1;
+                            Debug.Log("Lever 3-4 Hij staat Uit");
+                            Vector3 newPosition1 = new Vector3(Lever3.position.x, Lever3.position.y, zValue2);
+                            Lever3.position = newPosition1;
 
-                            Vector3 newPosition2 = new Vector3(Lever2.position.x, Lever2.position.y, zValue);
-                            Lever2.position = newPosition2;
+                            Vector3 newPosition2 = new Vector3(Lever4.position.x, Lever4.position.y, zValue);
+                            Lever4.position = newPosition2;
                             //lever.SetActive(false);             
                         }        
 
                 }
         }
 
-            isLeverOn = !isLeverOn;    
+            isLeverOn = !isLeverOn;
                          
         }   
     }
