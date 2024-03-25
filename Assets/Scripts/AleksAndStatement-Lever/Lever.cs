406a8f83390b30ffddf86df8d375d83b3a9ba830
Aleksander2006 <aleksvansanten@gmail.com>
2024-03-25 22:15:50 +0100

    Trigger veranderd,
    alles werkt behalve de twee knoppen die je moet gebruiken.

--- Assets/Scripts/AleksAndStatement-Lever/Lever.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Lever.cs
@@ -31,58 +31,58 @@ public class Lever : MonoBehaviour
         if(other.tag == "Character") {
             Debug.Log("Inside");
             IsCharacterInside = true;
-        } else {
-            IsCharacterInside = false;
-        }
+        } //else {
+           // IsCharacterInside = false;
+        //}
     }
 
     void OnTriggerExit2D(Collider2D other) {
         if(other.tag == "Character") {
             Debug.Log("Outside");
             IsCharacterInside = false;
-        }
+        } //else {
+           // IsCharacterInside = false;
+        //}
     }
             
     void Update() {
         if (IsCharacterInside){
-                Debug.Log("Character is binnnen");
-                if(Input.GetKeyDown(KeyCode.E)) { // gebruik de toets 'E' voor lever1
-                    Debug.Log("Ingedrukt");
-                    if(!isLeverOn) {
-                        Debug.Log("Lever 1 staat AAN");
-                        lever1.transform.localRotation = Quaternion.Euler(0, 180, 180);
-                    } else {
-                        Debug.Log("Lever 1 staat UIT");
-                        lever1.transform.localRotation = Quaternion.Euler(0, 0, 0);   
-                    }
-                    isLeverOn = !isLeverOn;
+            Debug.Log("Character is binnnen");
+            if(Input.GetKeyDown(KeyCode.E)) { // gebruik de toets 'E' voor lever1
+                Debug.Log("Ingedrukt");
+                if(!isLeverOn) {
+                    Debug.Log("Lever 1 staat AAN");
+                    lever1.transform.localRotation = Quaternion.Euler(0, 180, 180);
+                } else {
+                    Debug.Log("Lever 1 staat UIT");
+                    lever1.transform.localRotation = Quaternion.Euler(0, 0, 0);   
                 }
+                isLeverOn = !isLeverOn;
+            }
 
-                if(Input.GetKeyDown(KeyCode.F)) { // gebruik de toets 'F' voor lever3
-                    if(!isLeverOn2) {
-                        Debug.Log("Lever 2 staat AAN");
-                        lever3.transform.localRotation = Quaternion.Euler(0, 180, 180);
-                    } else {
-                        Debug.Log("Lever 2 staat UIT");
-                        lever3.transform.localRotation = Quaternion.Euler(0, 0, 0);             
-                    }
-                    isLeverOn2 = !isLeverOn2;
+            if(Input.GetKeyDown(KeyCode.F)) { // gebruik de toets 'F' voor lever3
+                if(!isLeverOn2) {
+                    Debug.Log("Lever 2 staat AAN");
+                    lever3.transform.localRotation = Quaternion.Euler(0, 180, 180);
+                } else {
+                    Debug.Log("Lever 2 staat UIT");
+                    lever3.transform.localRotation = Quaternion.Euler(0, 0, 0);             
                 }
-                
-                //beide levers moeten aanstaan om het lampje te laten branden
-                if(!isLeverOn && !isLeverOn2){
-                    //Grijs lampje gaat weg,
-                    Lampje1.SetActive(false);
-                    Debug.Log("Lamp AAN");
+                isLeverOn2 = !isLeverOn2;
+            }
+            
+            //beide levers moeten aanstaan om het lampje te laten branden
+            if(!isLeverOn && !isLeverOn2){
+                //Grijs lampje gaat weg,
+                Lampje1.SetActive(false);
+                Debug.Log("Lamp AAN");
 
-                } else { 
-                    //Grijs lampje blijf
-                    Lampje1.SetActive(true);
-                    Debug.Log("Lamp UIT");      
-                }  
-            } else {
-            IsCharacterInside = false;
-        } 
+            } else { 
+                //Grijs lampje blijf
+                Lampje1.SetActive(true);
+                Debug.Log("Lamp UIT");      
+            }  
+        }  
     }
 }  
 
--- Assets/Scripts/AleksAndStatement-Lever/Samen.cs
+++ Assets/Scripts/AleksAndStatement-Lever/Samen.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class Samen : MonoBehaviour
 {
-    public GameObject Lucas;
-    public GameObject Aleks;
-    public GameObject lampjeCheck;
+    //public GameObject Lucas;
+    //public GameObject Aleks;
+    //public GameObject lampjeCheck;
 
-    
-
-    // Start is called before the first frame update
-    void Start()
-    { 
-           
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-        if(Lucas && Aleks == true){
-        
-        lampjeCheck.SetActive(false);
-
-       } 
-    }
+//    GameObject Lucas = GameObject.Find("LampjeOff");
+//    GameObject Aleks = GameObject.Find("Lampje1");
+//    GameObject middelsteLampje = GameObject.Find("lampje1Check");
+//    
+//
+//
+//    // Start is called before the first frame update
+//    void Start()
+//    { 
+//   
+//    }
+//
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        
+//        if (Lucas.activeSelf && Aleks.activeSelf)
+//        {
+//            middelsteLampje.SetActive(true);
+//        }
+//        else
+//        {
+//            middelsteLampje.SetActive(false);
+//        } 
+//    }
 }
