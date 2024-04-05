957dbcf43a29800d4ffbccb6b2b3d40d8ebd3298
Aleksander2006 <aleksvansanten@gmail.com>
2024-04-05 09:14:06 +0200

    uodate

--- Assets/Scripts/AndStatement-Lever/LampAanUit.cs
+++ Assets/Scripts/AndStatement-Lever/LampAanUit.cs
@@ -11,22 +11,22 @@ public class LampAanUit : MonoBehaviour
     void Start()
     {
         lever1Script = GameObject.FindWithTag("Lever1").GetComponent<Lever_1>();
-        lever2Script = GameObject.FindWithTag("Lever3").GetComponent<Lever_2>();
+        lever2Script = GameObject.FindWithTag("Lever3").GetComponent<Lever_2>();   
     }
 
     
     void Update()
     {
-      //beide levers moeten aanstaan om het lampje te laten branden
+        //beide levers moeten aanstaan om het lampje te laten branden
         if(lever1Script.isLeverOn == false && lever2Script.isLeverOn2 == false){
 
-            //Grijs lampje gaat weg,
+            //Grijs lampje gaat weg
             Lampje1.SetActive(false);
             Debug.Log("Lamp AAN");
 
         } else {
 
-            //Grijs lampje blijf
+            //Grijs lampje blijft
             Lampje1.SetActive(true);
             Debug.Log("Lamp UIT");      
         } 
