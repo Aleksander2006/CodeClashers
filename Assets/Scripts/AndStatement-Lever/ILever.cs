e68cfb8fdc4e8108ecf763f1326074ba827e82f8
Aleksander2006 <aleksvansanten@gmail.com>
2024-04-07 11:10:51 +0200

    update

--- Assets/Scripts/AndStatement-Lever/ILever.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ILever
-{
-    //public bool isLeverOn = true;
-    //public bool isLeverOn2 = true; 
-}
