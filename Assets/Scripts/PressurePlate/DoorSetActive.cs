1441db359b79ba8c889fb93861a4d9f968c88d1f
EnricoScatolin <enrico.magalhaesscatolin@student.hu.nl>
2024-04-12 10:07:07 +0200

    o

--- Assets/Scripts/PressurePlate/DoorSetActive.cs
+++ Assets/Scripts/PressurePlate/DoorSetActive.cs
@@ -3,59 +3,71 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class DoorSetActive : MonoBehaviour, IDoor {
+public class DoorSetActive : MonoBehaviour, IDoor
+{
 
     private bool isDoorOpen = false;
     public bool isDoorOpen1 = false;
     public bool isDoorOpen2 = false;
     public bool isLampOn2 = false;
+    [SerializeField] private Animator DoorOpen;
 
-    public void Update() {
+    public void Update()
+    {
         DoorSignal3();
     }
 
     //Hide de closed door
-    public void OpenDoor() {
+    public void OpenDoor()
+    {
         isDoorOpen = true;
-        gameObject.SetActive(false);
+        DoorOpen.SetBool("AnimPlayer", true);
     }
 
     //Showd de closed door
-    public void CloseDoor() {
+    public void CloseDoor()
+    {
         isDoorOpen = false;
         gameObject.SetActive(true);
     }
 
     //AND levers signaal voor de deur
-    public void DoorSignal1() {
+    public void DoorSignal1()
+    {
         isDoorOpen1 = true;
         gameObject.SetActive(true);
     }
 
     //PressurePlate signaal voor de deur
-    public void DoorSignal2() {
+    public void DoorSignal2()
+    {
         isDoorOpen2 = true;
         gameObject.SetActive(true);
     }
 
     //Reset de values van signalen van AND lampje
-    public void DoorSignalClose1() {
+    public void DoorSignalClose1()
+    {
         isDoorOpen1 = false;
         isDoorOpen = false;
     }
 
     //Reset de values van signalen van PressurePlate lampje
-    public void DoorSignalClose2() {
+    public void DoorSignalClose2()
+    {
         isDoorOpen2 = false;
         isDoorOpen = false;
     }
 
     //Checkt of de signalen van bijde lampjes aan zijn, opent dan de deur
-    public void DoorSignal3() {
-        if(isDoorOpen1 && isDoorOpen2){
-            if(!isDoorOpen){
+    public void DoorSignal3()
+    {
+        if (isDoorOpen1 && isDoorOpen2)
+        {
+            if (!isDoorOpen)
+            {
                 OpenDoor();
             }
         }
-    } 
+    }
 }
