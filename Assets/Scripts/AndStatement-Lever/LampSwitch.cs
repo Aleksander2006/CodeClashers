b3ff6bdbb8e36cc1195872eba6edc1e6411abf2b
SuperGamer-NL <archeolooglucas@gmail.com>
2024-04-18 11:12:56 +0200

    lampje kan aan en uit, en zit aan zowel AND als Deur verbonden

--- Assets/Scripts/AndStatement-Lever/LampSwitch.cs
+++ Assets/Scripts/AndStatement-Lever/LampSwitch.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LampSwitch : MonoBehaviour, Ilamp {
+   [SerializeField] private GameObject doorGameObject;
+    private IDoor door;
+    private void Awake() {
+        door = doorGameObject.GetComponent<IDoor>();
+    }
 
     private bool isLeverOn1 = false;
-    
+
     private bool isLeverOn2 = false;
 
     public void leverOnSignal1() {
@@ -23,7 +28,6 @@ public class LampSwitch : MonoBehaviour, Ilamp {
     }
 
     public void leverOffSignal1() {
-        
         if (isLeverOn1 == true) {
             isLeverOn1 = false;
             Debug.Log("Lever1 = FALSE");
@@ -32,7 +36,6 @@ public class LampSwitch : MonoBehaviour, Ilamp {
     }
 
     public void leverOffSignal2() {
-        
         if (isLeverOn2 == true) {
             isLeverOn2 = false;
             Debug.Log("Lever2 = FALSE");
@@ -57,11 +60,13 @@ public class LampSwitch : MonoBehaviour, Ilamp {
      public void Update() {
             if (isLeverOn1 == true && isLeverOn2 == true){
                 gameObject.SetActive(false);
+                door.DoorSignal1();
                 Debug.Log("lamp AAN");
             }
             if (isLeverOn1 == false || isLeverOn2 == false){
                 gameObject.SetActive(true);
                 Debug.Log("lamp UIT");
+                door.DoorSignalClose1();
             }
         } 
     }
