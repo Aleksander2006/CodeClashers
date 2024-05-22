32c645b6b7c5b41f960deae1f071a1e01d5ddc89
SuperGamer-NL <archeolooglucas@gmail.com>
2024-05-22 11:22:36 +0200

    spawnpoints erin gezet

--- Assets/Scripts/Character/SpawnScript.cs
+++ Assets/Scripts/Character/SpawnScript.cs
@@ -25,43 +25,39 @@ public class SpawnScript : MonoBehaviour
         StaticData.floatToKeep = spawnLocation;
 
         if(spawnLocation == "teleporter_0a") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(-0.83f, -2.6f, -5);
         } 
 
         if(spawnLocation == "teleporter_1a") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_0a.transform.position + new Vector3(0, 3, -5);
         } 
 
         if(spawnLocation == "teleporter_1b") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_2a.transform.position + new Vector3(0, 1.2f, -5);
         } 
 
         if(spawnLocation == "teleporter_1c") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_3a.transform.position + new Vector3(0, 1.2f, -5);
         } 
 
         if(spawnLocation == "teleporter_1d") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_4a.transform.position + new Vector3(0, 1.2f, -5);
         } 
 
         if(spawnLocation == "teleporter_2a") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_1b.transform.position + new Vector3(-0.93f, -2.7f, -5);
         } 
 
         if(spawnLocation == "teleporter_2b") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_1b.transform.position + new Vector3(-0.93f, -2.7f, -5);
         } 
 
         if(spawnLocation == "teleporter_3a") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
-        } 
-
-        if(spawnLocation == "teleporter_3b") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_1c.transform.position + new Vector3(-0.93f, -2.7f, -5);
         } 
 
         if(spawnLocation == "teleporter_4a") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(0, -1.5f, 0);
+            gameObject.transform.position = teleporter_1c.transform.position + new Vector3(-0.93f, -2.7f, -5);
         } 
     }
 
