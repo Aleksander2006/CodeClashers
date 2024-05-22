0467c2265c529c9bb8c0abe69a77ce609827a81f
Aleksander2006 <aleksvansanten@gmail.com>
2024-05-22 16:56:28 +0200

    Int WaterLayer werkt

--- Assets/Scripts/Schuifknop/minMaxerInt.cs
+++ Assets/Scripts/Schuifknop/minMaxerInt.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 using Unity.VisualScripting;
@@ -21,7 +22,11 @@ public class minMaxer : MonoBehaviour
     Vector2 movement;
 
     public bool status = false;
-    public float speed = 0;
+    //public float speed = 0.012f;
+
+    private int counter = 10;
+    private float timer = 0;
+    private float delay = 0.5f; // 0,5 seconde vertraging
 
     [SerializeField] GameObject floatWaterLayer;
 
@@ -32,24 +37,65 @@ public class minMaxer : MonoBehaviour
         floatWaterLayer.GetComponent<Transform>();
     }
 
-    public void ScaleLayer (){
+    public void ScaleLayer (){ //Check of Schuifknop AANstaat
         if (status == true){
-            //StartCoroutine(FadeDelay());
-            IntLayer();
+            StartCoroutine(FadeDelay());
         }
     }
 
-    // private IEnumerator FadeDelay() {
-    //     yield return new WaitForSeconds(6);
+    private IEnumerator FadeDelay() {
+        yield return new WaitForSeconds(0.5f);
+        IntLayer();
+    }
+
+    private void IntLayer() {
+        if (timer > delay) {
+            timer = 0f;
+            if (counter >= 1) {
+                floatWaterLayer.transform.localScale = transform.TransformVector(counter, counter, counter) - new Vector3(1, 1, 0);
+                counter--;
+            }
+        }
+    }
+
+
+    
+
+    // private void IntLayer() { //Zorgt ervoor dat de WaterLayer kleiner wordt
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(10, 10, 10) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+        
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(9, 9, 9) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+        
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(8, 8, 8) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(7, 7, 7) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(6, 6, 6) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(5, 5, 5) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(4, 4, 4) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(3, 3, 3) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(2, 2, 2) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     floatWaterLayer.transform.localScale = transform.TransformVector(1, 1, 1) - new Vector3(1, 1, 0);
+    //     StartCoroutine(FadeDelay2());
+
+    //     // if (floatWaterLayer.transform.localScale.x <= 0 && floatWaterLayer.transform.localScale.y <= 0) {
+    //     //     floatWaterLayer.transform.localScale = Vector2.zero;
+    //     // }
     // }
-
-    public void IntLayer() {
-        floatWaterLayer.transform.localScale -= new Vector3(0.7f, 0.7f, 0) * speed;
-
-        if (floatWaterLayer.transform.localScale.x <= 0f && floatWaterLayer.transform.localScale.y <= 0f) {
-            floatWaterLayer.transform.localScale = Vector3.zero;
-        }
-    }
 
     //------------Schuifknop Functionaliteit-------------
 
@@ -95,15 +141,20 @@ public class minMaxer : MonoBehaviour
     }
 
     void FixedUpdate(){
+       
         ScaleLayer();
         posLimiter();
         posLimitRight();
         posLimitLeft();
         if(colliding == true) {
             RigidBodyLink.MovePosition(RigidBodyLink.position + movement * MoveSpeed * Time.fixedDeltaTime);
-            Debug.Log("Movable...");
+            //Debug.Log("Movable...");
         } else {
-            Debug.Log("Not Movable...");
+            //Debug.Log("Not Movable...");
         }
     } 
+
+    void Update (){
+        timer += Time.deltaTime;
+    }
 }
