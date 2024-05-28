2712cc9bed427b5a053bddf6697bd863ea556c0e
EnricoScatolin <enrico.magalhaesscatolin@student.hu.nl>
2024-05-28 12:50:54 +0200

    i

--- Assets/FloatMechanic.cs
+++ Assets/FloatMechanic.cs
@@ -40,100 +40,92 @@ public class LegeSchuifknopScript : MonoBehaviour
     { //Check of Schuifknop AANstaat
         if (status == true)
         {
-            StartCoroutine(FadeDelay());
-        }
-    }
-
-    private IEnumerator FadeDelay()
-    { //Zorgt ervoor dat de Schuifknop wacht voor 0,7 seconden en dan doorgaat.
-        yield return new WaitForSeconds(0.7f);
-        FloatLayer();
-    }
-
-    private void FloatLayer()
-    { // De functie die ervoor zorgt dat de Waterlayer steeds -1 downscaled
-        if (timer > delay)
-        {
-            timer = 0f;
-            if (counter >= 1)
-            {
-                FloatWaterLayer.transform.localScale = transform.TransformVector(counter, counter2, 0) - new Vector3(0.842517f, 0.596645f, 0);
-                counter--;
-                counter2--;
-            }
-        }
-    }
-
-    //------------Schuifknop Functionaliteit-------------
-
-    public void posLimiter()
-    {
-        if (gameObject.transform.position.x > (startPosX + 0.715f) || transform.position.x < (startPosX - 0.715f))
-        {
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
-            movable = false;
-
-        }
-        else
-        {
-            if (colliding == true)
-            {
-                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                movable = true;
-            }
-        }
-    }
-
-    public void posLimitRight()
-    {
-        if (gameObject.transform.position.x > (startPosX + 0.715f))
-        {
-            if (colliding == false)
-            {
-                gameObject.transform.position = new Vector3(startPosX + 0.71f, startPosY, -3.24f);
-            }
-        }
-    }
-
-    public void posLimitLeft()
-    {
-        if (gameObject.transform.position.x < (startPosX - 0.715f))
-        {
-            if (colliding == false)
-            {
-                gameObject.transform.position = new Vector3(startPosX - 0.71f, startPosY, -3.24f);
-            }
-        }
-    }
-
-    private void OnTriggerEnter2D()
-    {
-        colliding = true;
-    }
-
-    private void OnTriggerStay2D()
-    {
-        colliding = true;
-    }
-
-    private void OnTriggerExit2D()
-    {
-        colliding = false;
-    }
-
-    void FixedUpdate()
-    {
-        posLimiter();
-        posLimitRight();
-        posLimitLeft();
-        if (colliding == true)
-        {
-            RigidBodyLink.MovePosition(RigidBodyLink.position + movement * MoveSpeed * Time.fixedDeltaTime);
-            Debug.Log("Movable...");
-        }
-        else
-        {
-            Debug.Log("Not Movable...");
+            FloatLayer();
         }
     }
 }
+
+
+
+private void FloatLayer()
+{ // De functie die ervoor zorgt dat de Waterlayer steeds -1 downscaled
+
+    FloatWaterLayer.transform.localScale = transform.TransformVector(counter, counter2, 0) - new Vector3(0.842517f, 0.596645f, 0);
+
+
+}
+
+//------------Schuifknop Functionaliteit-------------
+
+public void posLimiter()
+{
+    if (gameObject.transform.position.x > (startPosX + 0.715f) || transform.position.x < (startPosX - 0.715f))
+    {
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        movable = false;
+
+    }
+    else
+    {
+        if (colliding == true)
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            movable = true;
+        }
+    }
+}
+
+public void posLimitRight()
+{
+    if (gameObject.transform.position.x > (startPosX + 0.715f))
+    {
+        if (colliding == false)
+        {
+            gameObject.transform.position = new Vector3(startPosX + 0.71f, startPosY, -3.24f);
+        }
+    }
+}
+
+public void posLimitLeft()
+{
+    if (gameObject.transform.position.x < (startPosX - 0.715f))
+    {
+        if (colliding == false)
+        {
+            gameObject.transform.position = new Vector3(startPosX - 0.71f, startPosY, -3.24f);
+        }
+    }
+}
+
+private void OnTriggerEnter2D()
+{
+    colliding = true;
+}
+
+private void OnTriggerStay2D()
+{
+    colliding = true;
+}
+
+private void OnTriggerExit2D()
+{
+    colliding = false;
+}
+
+void FixedUpdate()
+{
+    ScaleLayer();
+    posLimiter();
+    posLimitRight();
+    posLimitLeft();
+    if (colliding == true)
+    {
+        RigidBodyLink.MovePosition(RigidBodyLink.position + movement * MoveSpeed * Time.fixedDeltaTime);
+        Debug.Log("Movable...");
+    }
+    else
+    {
+        Debug.Log("Not Movable...");
+    }
+}
+
