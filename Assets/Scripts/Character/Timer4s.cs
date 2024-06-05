abeadceb9f866279303c4bfed6f9df93c2eb9034
SuperGamer-NL <archeolooglucas@gmail.com>
2024-06-05 15:15:49 +0200

    DrownTimer

--- Assets/Scripts/Character/Timer4s.cs
+++ Assets/Scripts/Character/Timer4s.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.U2D.Animation;
@@ -12,6 +14,8 @@ public class Timer4s : MonoBehaviour
 
     private float timer = 4f;
     private bool TimerCheck = false;
+    private string timerString;
+    private float timerRounded;
     public Animator Crossfade;
 
     [SerializeField] TransitionTrigger transitionTrigger;
@@ -19,8 +23,11 @@ public class Timer4s : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] MovementScript movementScript;
 
+    [SerializeField] TextMeshProUGUI redTimer;
+
     void Start(){
         transitionTrigger.GetComponent<TransitionTrigger>();
+        redTimer.enabled = false;
     }
     
     private void OnTriggerEnter2D (Collider2D other){
@@ -54,23 +61,37 @@ public class Timer4s : MonoBehaviour
     }
 
     void Update()
-    {       
+    {      
         if (TimerCheck == true){
-
             Timer();
         }
+
         if (TimerCheck == false){
             timer = 4;
         }
+
         if (timer <= 0){ 
             StartCoroutine(FadeDelay());
             Crossfade.SetTrigger("Go");
+            timer = 0f;
         }
 
         if(timer < 1.75) {
             animator.SetBool("DrownPlayer", true);
             animator.SetFloat("Speed", 0.001f);
         }
+
+        if(timer < 4) {
+            redTimer.enabled = true;
+        }
+
+        if(timer > 3.99) {
+            redTimer.enabled = false;
+        }
+
+        timerRounded = (float)Math.Round(timer, 1);
+        timerString = timerRounded.ToString();
+        redTimer.text = timerString;
     }
 }
 
