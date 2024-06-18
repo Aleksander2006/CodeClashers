d943ed96b74a7f23b2117e3cd0422d257b611197
Aleksander2006 <aleksvansanten@gmail.com>
2024-06-18 14:51:47 +0200

    Build settings aangepast en play en quit functie

---
+++ Assets/Scripts/StartMenu/PlayFunction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayFunction : MonoBehaviour
+{
+    public void Playfunction(){
+        SceneManager.LoadScene(1);
+    }
+}
