b9133b00bd27cd1f4f49328ff0deb66ad07415fc
Aleksander2006 <aleksvansanten@gmail.com>
2024-06-18 16:01:28 +0200

    Play knop werkt half

--- Assets/Scripts/StartMenu/PlayFunction.cs
+++ Assets/Scripts/StartMenu/PlayFunction.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayFunction : MonoBehaviour
 {
+
+    public Animator Crossfade;
+
+
+
+    void Start (){
+        //Crossfade.SetBool("Start", false);
+        //Crossfade.enabled = false;
+    }
+
     public void Playfunction(){
-        SceneManager.LoadScene(1);
+        Debug.Log("PRESSED");
+
+        StartCoroutine(FadeDelay());
+        Crossfade.SetTrigger("Go");
+        //Crossfade.SetBool("Start", true);
+        //SceneManager.LoadScene(1);  
+    }
+
+    public IEnumerator FadeDelay() {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 }
