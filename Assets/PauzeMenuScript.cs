aabef1ca9238db9663570ac3f0dc4e49a5cf67a7
SuperGamer-NL <archeolooglucas@gmail.com>
2024-06-18 15:48:19 +0200

    Escape freezed game en toggled menu placeholder

---
+++ Assets/PauzeMenuScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class PauzeMenuScript : MonoBehaviour {
+    
+    [SerializeField] GameObject mainCharacter;
+    [SerializeField] GameObject pauzeMenu;
+    private static bool escPressed = false;
+
+
+    private void EscToggle() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            escPressed = !escPressed;
+        } 
+    }
+
+    private void Pauzer() {
+        if(escPressed == true) {
+            Time.timeScale = 0f;
+            mainCharacter.GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    private void Resumer() {
+        if(escPressed == false) {
+            Time.timeScale = 1f;
+            mainCharacter.GetComponent<Animator>().enabled = true;
+        }
+    }
+
+    private void MenuToggle() {
+        if(escPressed == false) {
+            pauzeMenu.SetActive(false);
+        }
+
+        if(escPressed == true) {
+            pauzeMenu.SetActive(true);
+        }
+    }
+
+
+    void Update() {
+        EscToggle();
+        Pauzer();
+        Resumer();
+        MenuToggle();
+        Debug.Log("EscPressed is " + escPressed);
+    }
+}
+
+
+/// misschien kan ik wel een scene maken met een gO die dit soort functies in een interface hebben staan, waar je dan bij kan, en dus dit kan uitvoeren in welke scene dan ook, deze scene moet je dan niet kunnen uitzetten, deze moet altijd aan zijn... zegamaar een menu toggler.
