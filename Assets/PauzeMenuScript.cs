81fa7bd5523d748d6c7fce21852aab3475bb60c6
SuperGamer-NL <archeolooglucas@gmail.com>
2024-06-20 15:29:00 +0200

    audio fix en quit knop fix

--- Assets/PauzeMenuScript.cs
+++ Assets/PauzeMenuScript.cs
@@ -40,6 +40,7 @@ public class PauzeMenuScript : MonoBehaviour {
         if(escPressed == true) {
             Time.timeScale = 0f;
             mainCharacter.GetComponent<Animator>().enabled = false;
+            mainCharacter.GetComponent<AudioSource>().enabled = false;
         }
     }
 
@@ -47,6 +48,7 @@ public class PauzeMenuScript : MonoBehaviour {
         if(escPressed == false) {
             Time.timeScale = 1f;
             mainCharacter.GetComponent<Animator>().enabled = true;
+            mainCharacter.GetComponent<AudioSource>().enabled = false;
         }
     }
 
