bc7af017962f9441270f0c24c8a6a5c1cefccb7a
SuperGamer-NL <archeolooglucas@gmail.com>
2024-06-20 15:50:31 +0200

    audio fix 4 real

--- Assets/PauzeMenuScript.cs
+++ Assets/PauzeMenuScript.cs
@@ -8,6 +8,7 @@ using UnityEngine.TextCore.Text;
 public class PauzeMenuScript : MonoBehaviour {
     
     [SerializeField] GameObject mainCharacter;
+    [SerializeField] GameObject backGroundAudio;
     [SerializeField] GameObject pauzeMenu;
     [SerializeField] GameObject resumeButton;
     [SerializeField] GameObject quitButton;
@@ -34,13 +35,18 @@ public class PauzeMenuScript : MonoBehaviour {
 
     public void ResumeButtonFunction() {
             escPressed = !escPressed;
+            mainCharacter.GetComponent<AudioSource>().enabled = true;
+            backGroundAudio.GetComponent<AudioSource>().Play();
     }
 
     private void Pauzer() {
         if(escPressed == true) {
             Time.timeScale = 0f;
             mainCharacter.GetComponent<Animator>().enabled = false;
-            mainCharacter.GetComponent<AudioSource>().Stop();
+            if(Input.GetKeyDown(KeyCode.Escape)) {
+            mainCharacter.GetComponent<AudioSource>().enabled = false;
+            backGroundAudio.GetComponent<AudioSource>().Pause();
+            }
         }
     }
 
@@ -48,7 +54,10 @@ public class PauzeMenuScript : MonoBehaviour {
         if(escPressed == false) {
             Time.timeScale = 1f;
             mainCharacter.GetComponent<Animator>().enabled = true;
-            mainCharacter.GetComponent<AudioSource>().Play();
+            if(Input.GetKeyDown(KeyCode.Escape)) {
+            mainCharacter.GetComponent<AudioSource>().enabled = true;
+            backGroundAudio.GetComponent<AudioSource>().Play();
+            }
         }
     }
 
