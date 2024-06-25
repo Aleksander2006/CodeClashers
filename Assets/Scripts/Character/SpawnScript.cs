53235431499e37c8fcf948bd983c58fcc694a02a
SuperGamer-NL <archeolooglucas@gmail.com>
2024-06-25 10:16:36 +0200

    poppetje overlap fix

--- Assets/Scripts/Character/SpawnScript.cs
+++ Assets/Scripts/Character/SpawnScript.cs
@@ -25,7 +25,7 @@ public class SpawnScript : MonoBehaviour
         StaticData.floatToKeep = spawnLocation;
 
         if(spawnLocation == "teleporter_0a") {
-            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(-0.83f, -2.6f, -5);
+            gameObject.transform.position = teleporter_1a.transform.position + new Vector3(-0.83f, -2.6f, -3.59f);
         } 
 
         if(spawnLocation == "teleporter_1a") {
@@ -45,19 +45,19 @@ public class SpawnScript : MonoBehaviour
         } 
 
         if(spawnLocation == "teleporter_2a") {
-            gameObject.transform.position = teleporter_1b.transform.position + new Vector3(-0.93f, -2.7f, -5);
+            gameObject.transform.position = teleporter_1b.transform.position + new Vector3(-0.93f, -2.7f, -3.59f);
         } 
 
         if(spawnLocation == "teleporter_2b") {
-            gameObject.transform.position = teleporter_1b.transform.position + new Vector3(-0.93f, -2.7f, -5);
+            gameObject.transform.position = teleporter_1b.transform.position + new Vector3(-0.93f, -2.7f, -3.59f);
         } 
 
         if(spawnLocation == "teleporter_3a") {
-            gameObject.transform.position = teleporter_1c.transform.position + new Vector3(-0.93f, -2.7f, -5);
+            gameObject.transform.position = teleporter_1c.transform.position + new Vector3(-0.93f, -2.7f, -3.59f);
         } 
 
         if(spawnLocation == "teleporter_4a") {
-            gameObject.transform.position = teleporter_1c.transform.position + new Vector3(-0.93f, -2.7f, -5);
+            gameObject.transform.position = teleporter_1c.transform.position + new Vector3(-0.93f, -2.7f, -3.59f);
         } 
     }
 
